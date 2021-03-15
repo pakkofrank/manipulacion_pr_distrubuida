c69482f59bf645beabcf584ef17433eac7bce77d
pakkofrank <jpalma@ucol.mx>
2021-03-14 23:18:11 -0600

    asi nomas quedo xD

--- src/main/java/Manipulacion/App.java
+++ src/main/java/Manipulacion/App.java
@@ -1,16 +1,19 @@
 package Manipulacion;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+import java.io.*;
 
 public class App {
     public static void  main (String[] args ){
 
         File myFile;
-        myFile = new File("filename4.txt");
+        myFile = new File("filename.txt");
         createFile(myFile);
-        writeContent(myFile, "hola mundo" );
+        writeContent(myFile, "que paso master" );
+        Route(myFile);
+        ReaderFile(myFile);
 
     }
 
@@ -24,7 +27,6 @@ public class App {
         } finally{
 
         }
-
     }
 
     public static void writeContent(File myFile, String contents){
@@ -40,5 +42,38 @@ public class App {
         }
     }
 
+    public static void Route(File targetFile){
+        try {
+            System.out.println("route: " + targetFile.getName() + targetFile.getAbsolutePath());
+        } catch (SecurityException e) {
+            System.out.println("AN error occurred: " + e.getMessage());
+        }
+    }
 
+    public static void ReaderFile(File targetFile){
+        FileReader reader = null;
+        BufferedReader buffer = null;
+
+        try{
+            reader = new FileReader(targetFile);
+            buffer = new BufferedReader(reader);
+
+            String line;
+            System.out.println("reader." + targetFile.getName());
+
+            while((line=buffer.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch(IOException e){
+            System.out.println("Error alert" + e.getMessage());
+        }finally{
+            try{
+                if( null != reader ){
+                    reader.close();
+                }
+            }catch (Exception e1){
+                System.out.println("Error reader" + e1.getMessage());
+            }
+        }
+    }
 }
