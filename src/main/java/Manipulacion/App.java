ebfb9c56b55dda2ff63caf5ce3d97aa18f78dcb0
pakkofrank <jpalma@ucol.mx>
2021-03-14 22:27:27 -0600

    procedimiento aumentado

--- src/main/java/Manipulacion/App.java
+++ src/main/java/Manipulacion/App.java
@@ -1,12 +1,14 @@
 package Manipulacion;
 
-import java.in.File;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class App {
     public static void  main (String[] args ){
 
         File myFile;
-        myFile = new File("filename3.txt");
+        myFile = new File("filename4.txt");
         createFile(myFile);
         writeContent(myFile, "hola mundo" );
 
@@ -17,10 +19,25 @@ public class App {
             if (myFile.createNewFile()){
                 System.out.println("File create: " + myFile.getName());
             }
-        }catch (IDException e){
+        }catch (IOException e){
             System.out.println("AN error occurred: " + e.getMessage());
+        } finally{
+
         }
 
+    }
+
+    public static void writeContent(File myFile, String contents){
+        FileWriter writer = null;
+
+        try {
+            writer = new FileWriter(myFile);
+            writer.write(contents);
+
+            writer.close();
+        } catch(IOException e){
+            System.out.println("AN error occurred while writing: " + e.getMessage());
+        }
     }
 
 
